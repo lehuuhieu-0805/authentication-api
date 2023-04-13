7f55d94a923f2e33106c606fd0316d2c0f293938
lehuuhieu-0805 <01643815296h@gmail.com>
2023-04-13 17:43:55 +0700

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Authentication API')
+    .setDescription('The Authentication API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => {
