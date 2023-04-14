dee18ad96656829fc4dff3c47ea9f053d1abaad5
lehuuhieu-0805 <01643815296h@gmail.com>
2023-04-14 23:29:10 +0700

    add config jwt

--- src/components/auth/auth.module.ts
+++ src/components/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/user.entity';
 import { UserModule } from '../user/user.module';
@@ -6,7 +7,15 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), UserModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '10m' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
